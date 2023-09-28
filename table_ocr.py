497ca0e532cc000ce7d15c021dbbac02b04c0a56
shuheik <shuheik@users.noreply.github.com>
2023-09-28 13:22:20 +0900

    Update table_ocr.py

--- table_ocr.py
+++ table_ocr.py
@@ -1,31 +1,23 @@
-import os
 import sys
 import json
+from pathlib import Path
 from paddleocr import PaddleOCR
 
-def main(path:str): 
-  image_dir = os.path.join(path, "tables")
-  images = os.listdir(image_dir)
-  
-  out_dir = os.path.join(path, "ocr_result")
-  if not os.path.exists(out_dir):
-    os.mkdir(out_dir)
-  
-  ocr = PaddleOCR(use_angle_cls=False, lang='en')
 
-  for image_file in images:
-    image_path = os.path.join(image_dir, image_file)
+def main(path: str):
+  image_dir = Path(path, "tables")
+  images = image_dir.glob("*.png")
+  ocr = PaddleOCR(use_angle_cls=False, lang='en')
+  for image in images:
+    image_path = str(image)
     results = ocr.ocr(image_path, cls=False)
-    
     boxes = []
     tokens = []
-    
     for result in results:
       bbox, pair = result
       boxes.append(bbox)
       tokens.append(pair[0])
-    
-    with open(os.path.join(out_dir, image_file.replace(".png", ".json")), 'w') as f:
+    with open(image_path.replace(".png", ".json"), 'w') as f:
       json.dump({"tokens": tokens, "bboxes": boxes}, f, indent=2)
 
 
